255cd12575b7611909917d85e70f9048d3a0d86e
suraj <suraj87parmar@gmail.com>
2024-08-27 17:08:35 +0530

    addition of cloudinary and multer files

--- src/app.js
+++ src/app.js
@@ -15,5 +15,11 @@ app.use(express,urlencoded({extended: true,limit:
 app.use(express.static("public"))
 app.use(cookieParser())
 
+//routes
+import userRouter from './routes/user.routes.js'
+
+
+//routers declaration
+app.use("/api/v1/users",userRouter)
 
 export {app}
