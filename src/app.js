968203ac596841d8fea30b90c0225b0a8610ae77
suraj <suraj87parmar@gmail.com>
2024-09-05 17:20:32 +0530

    fixing bugs

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import express, { urlencoded } from "express"
+import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 
@@ -10,16 +10,17 @@ app.use(cors({
 }))
 
 app.use(express.json({limit: "16kb"}))
-app.use(express,urlencoded({extended: true,limit:
-"16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
-//routes
+
+//routes import
 import userRouter from './routes/user.routes.js'
 
+//routes declaration
+app.use("/api/v1/users", userRouter)
 
-//routers declaration
-app.use("/api/v1/users",userRouter)
+// http://localhost:8000/api/v1/users/register
 
-export {app}
+export { app }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -9,12 +9,14 @@ router.route("/register").post(
         {
             name: "avatar",
             maxCount: 1
-        },
+        }, 
         {
             name: "coverImage",
             maxCount: 1
         }
     ]),
-    registerUser)
+    registerUser
+    )
+
 
 export default router
